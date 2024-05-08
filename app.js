d55cee54c4ffe8c0ada88c883f5f558b5ec26ffc
Swarnendu Ghosh <141937171+SwarnenduG07@users.noreply.github.com>
2024-05-08 14:45:09 +0000

    DB added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require("./db/db").connect()
 const express = require('express');
 const app = express()
  
--- db/db.js
+++ db/db.js
@@ -1,13 +1,16 @@
- const mongoosese = require('mongoose')
+ const mongoose = require('mongoose')
 
- const MONGODB_URL = process.env()
+ const { MONGODB_URL } = process.env
 
  exports.connect = () => {
+
     mongoose.connect(MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then()
+    .then(
+        console.log('database connected successfully')
+    )
     .catch((error) => {
         console.log('DB connection failed');
         console.log(error);
